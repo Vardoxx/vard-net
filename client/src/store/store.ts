b992bac816de127e3dccb44f58c4433831cefc49
vardox <maksgavrilov956@gmail.com>
2024-08-25 23:31:44 +0300

    main logic is done

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,16 +1,18 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { authApi } from "./services/auth.service";
 import tokenReducer from "./slices/token.slice"; // Убедитесь, что путь к файлу правильный
+import { newsApi } from "./services/news.service";
 
 const rootReducer = combineReducers({
   [authApi.reducerPath]: authApi.reducer,
+  [newsApi.reducerPath]: newsApi.reducer,
   token: tokenReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, newsApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- server/src/news/news.service.ts
+++ server/src/news/news.service.ts
@@ -9,31 +9,31 @@ export class NewsService {
     this.newsList = [
       {
         id: 1,
-        src: '1123123123123123123123',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3NMLvURzAhu3YoEw4ulaG0OEDyhawqwcEiw&s',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         tags: ['blogers'],
       },
       {
         id: 2,
-        src: '1123123123123123123123',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBCbAaHSD5GW3L0J1Y1RuG8u-XjkO5eTROjA&s',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         tags: ['policy'],
       },
       {
         id: 3,
-        src: '1123123123123123123123',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRQ1688U5qT69NA5r7WjzmJee6OGnGO89jBg&s',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         tags: ['game'],
       },
       {
         id: 4,
-        src: '1123123123123123123123',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfrT8Vjk9QJrwwSwwBUzizJ2vPTiZ9Y6lcA&s',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         tags: ['policy', 'blogers'],
       },
       {
         id: 5,
-        src: '1123123123123123123123',
+        src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGYxm30lxbrfH5IFWd3OB96rf4e3Uxv5clcQ&s',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         tags: ['policy', 'game'],
       },
